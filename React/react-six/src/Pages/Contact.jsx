eacf93de5d49ba189eb214a76ac22523c0ea6b3e
hiraekram10 <heraiqram123@gmail.com>
2026-01-25 11:57:32 +0500

    react router dom with custom nav and ant design nav

---
+++ React/react-six/src/Pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomNav from '../components/CustomNav'
+
+const Contact = () => {
+  return (
+    <div>
+   Contact</div>
+  )
+}
+
+export default Contact
---
+++ React/react-six/src/Pages/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CustomNav from '../components/CustomNav'
+
+const Product = () => {
+  return (
+    <div>
+
+   dfjkhdvbn
+
+        
+        </div>
+  )
+}
+
+export default Product
