f57fbd3f52a59f693468bc7f383ff4558072e712
hiraekram10 <heraiqram123@gmail.com>
2026-03-08 11:10:31 +0500

    redux async api fetch

---
+++ React/react14/src/config/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import CartReducer from './CartSlice'
+import ProductReducer from './ProductSlice'
+
+const store = configureStore({
+    reducer:{
+  
+   cart:CartReducer,
+   product: ProductReducer
+   
+   
+    }
+})
+
+export default store
